bfe944d7da5ea1a6e2e320ffc7b7af6e50978d51
IMora04 <ignmorper1@alum.us.es>
2024-05-05 12:38:41 +0200

    Caption model creation

---
+++ src/main/java/aiss/vimeominer/model/captions/Captions.java
@@ -0,0 +1,185 @@
+
+package aiss.vimeominer.model.captions;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "total",
+    "page",
+    "per_page",
+    "paging",
+    "data"
+})
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Captions {
+
+    @JsonProperty("total")
+    private Integer total;
+    @JsonProperty("page")
+    private Integer page;
+    @JsonProperty("per_page")
+    private Integer perPage;
+    @JsonProperty("paging")
+    private Paging paging;
+    @JsonProperty("data")
+    private List<CaptionList> data;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Captions() {
+    }
+
+    /**
+     * 
+     * @param total
+     * @param perPage
+     * @param data
+     * @param paging
+     * @param page
+     */
+    public Captions(Integer total, Integer page, Integer perPage, Paging paging, List<CaptionList> data) {
+        super();
+        this.total = total;
+        this.page = page;
+        this.perPage = perPage;
+        this.paging = paging;
+        this.data = data;
+    }
+
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Captions withTotal(Integer total) {
+        this.total = total;
+        return this;
+    }
+
+    @JsonProperty("page")
+    public Integer getPage() {
+        return page;
+    }
+
+    @JsonProperty("page")
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Captions withPage(Integer page) {
+        this.page = page;
+        return this;
+    }
+
+    @JsonProperty("per_page")
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    @JsonProperty("per_page")
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
+
+    public Captions withPerPage(Integer perPage) {
+        this.perPage = perPage;
+        return this;
+    }
+
+    @JsonProperty("paging")
+    public Paging getPaging() {
+        return paging;
+    }
+
+    @JsonProperty("paging")
+    public void setPaging(Paging paging) {
+        this.paging = paging;
+    }
+
+    public Captions withPaging(Paging paging) {
+        this.paging = paging;
+        return this;
+    }
+
+    @JsonProperty("data")
+    public List<CaptionList> getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(List<CaptionList> data) {
+        this.data = data;
+    }
+
+    public Captions withData(List<CaptionList> data) {
+        this.data = data;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public Captions withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Captions.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("total");
+        sb.append('=');
+        sb.append(((this.total == null)?"<null>":this.total));
+        sb.append(',');
+        sb.append("page");
+        sb.append('=');
+        sb.append(((this.page == null)?"<null>":this.page));
+        sb.append(',');
+        sb.append("perPage");
+        sb.append('=');
+        sb.append(((this.perPage == null)?"<null>":this.perPage));
+        sb.append(',');
+        sb.append("paging");
+        sb.append('=');
+        sb.append(((this.paging == null)?"<null>":this.paging));
+        sb.append(',');
+        sb.append("data");
+        sb.append('=');
+        sb.append(((this.data == null)?"<null>":this.data));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}

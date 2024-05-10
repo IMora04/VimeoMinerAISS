64739f26c9b2ba85472a947ca11a516f99b14644
Elena Fernández-Llebrez <116157269+elenallebrez@users.noreply.github.com>
2024-05-10 14:08:15 +0200

    Update CaptionService.java

--- src/main/java/aiss/vimeoMiner/service/CaptionService.java
+++ src/main/java/aiss/vimeoMiner/service/CaptionService.java
@@ -1,7 +1,33 @@
 package aiss.vimeoMiner.service;
 
-public class CaptionService {
+import aiss.vimeoMiner.model.captions.CaptionList;
+import aiss.vimeoMiner.model.captions.Captions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@Service("captionService")
+public class CaptionService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Captions getCaptions(String videoId){
+        String uri = "https://api.vimeo.com/videos/" + videoId + "/texttracks"; ;
+        HttpHeaders header = new HttpHeaders();
+        header.set("Authorization", "Bearer " + "5394a30ebd1c27d98804ed901a30358a");
+
+        HttpEntity<Captions> request = new HttpEntity<>(null, header);
+
+        ResponseEntity<Captions> response = restTemplate.exchange(uri, HttpMethod.GET, request, Captions.class);
+
+        return response.getBody();
+    }
 
 
 }

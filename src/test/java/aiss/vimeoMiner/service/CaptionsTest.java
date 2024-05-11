9136e837c2ca44d9c1137ca02bbbdbb1401c09ba
ALBERTO BERNARDOS <albberrui@alum.us.es>
2024-05-11 18:33:43 +0200

    VimeoMiner captions service and test

--- src/test/java/aiss/vimeoMiner/service/CaptionsTest.java
+++ src/test/java/aiss/vimeoMiner/service/CaptionsTest.java
@@ -4,15 +4,20 @@ import aiss.vimeoMiner.model.captions.Captions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration
 public class CaptionsTest {
     @Autowired
-    @Qualifier("CaptionService")
+    @Qualifier("captionService")
     CaptionService captionService;
 
+    @Test
     void getCaptions(){
         Captions captions = captionService.getCaptions("1234");
+        System.out.println(captions);
     }
 
 }
